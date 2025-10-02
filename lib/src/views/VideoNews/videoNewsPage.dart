a39a88db24e3ce2db52b22cebfe7279fc96e2781
Mujahid Islam <mbiplobe@gmail.com>
2025-10-02 10:04:07 +0600

    home view

--- lib/src/views/VideoNews/videoNewsPage.dart
+++ lib/src/views/VideoNews/videoNewsPage.dart
@@ -9,125 +9,148 @@ import 'package:news_app/src/views/homePage/widget/newsCard.dart';
 import 'package:news_app/src/theme/theme.dart';
 
 class VideoNewsPage extends ConsumerWidget {
-  String? type;
+  final String? type;
+
+  const VideoNewsPage({Key? key, this.type}) : super(key: key);
+
   Widget _headerNews(BuildContext context, Article article) {
     return InkWell(
-        onTap: () {
-          // BlocProvider.of<DetailBloc>(context)
-          //     .add(SelectNewsForDetail(article: article));
-          // Navigator.pushNamed(context, '/detail');
-          context.push(AppRoutes.NewsDetailPageRoute,extra: article);
-        },
-        child: Container(
-            width: MediaQuery.of(context).size.width * 6,
-            child: ClipRRect(
-                borderRadius: BorderRadius.circular(0),
-                child: Stack(
-                  alignment: Alignment.bottomCenter,
+      onTap: () {
+        context.push(AppRoutes.NewsDetailPageRoute, extra: article);
+      },
+      child: Container(
+        width: MediaQuery.of(context).size.width * 6,
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(0),
+          child: Stack(
+            alignment: Alignment.bottomCenter,
+            children: <Widget>[
+              article.urlToImage == null
+                  ? Container()
+                  : customImage(article.urlToImage ?? '', fit: BoxFit.fitWidth),
+              Container(
+                padding: const EdgeInsets.only(left: 20, right: 10, bottom: 20),
+                alignment: Alignment.bottomCenter,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.end,
                   children: <Widget>[
-                    article.urlToImage == null
-                        ? Container()
-                        : customImage(article.urlToImage??'', fit: BoxFit.fitWidth),
-                    Container(
-                      padding: EdgeInsets.only(left: 20, right: 10, bottom: 20),
-                      alignment: Alignment.bottomCenter,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: <Widget>[
-                          Text(article.title??'',
-                              style: AppTheme.h2Style.copyWith(
-                                  color:
-                                      Theme.of(context).colorScheme.onSurface)),
-                          Text(article.getTime(),
-                              style: AppTheme.subTitleStyle.copyWith(
-                                  color: Theme.of(context).disabledColor))
-                        ],
+                    Text(
+                      article.title ?? '',
+                      style: AppTheme.h2Style.copyWith(
+                        color: Theme.of(context).colorScheme.onSurface,
                       ),
                     ),
-                    Align(
-                        alignment: Alignment.bottomRight,
-                        child: Padding(
-                          padding: EdgeInsets.all(10),
-                          child: _playWidget(context),
-                        ))
+                    Text(
+                      article.getTime(),
+                      style: AppTheme.subTitleStyle.copyWith(
+                        color: Theme.of(context).disabledColor,
+                      ),
+                    )
                   ],
-                ))));
+                ),
+              ),
+              Align(
+                alignment: Alignment.bottomRight,
+                child: Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: _playWidget(context),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 
+  /// Small play button widget
   Widget _playWidget(BuildContext context) {
     return SizedBox(
-        height: 25,
-        child: FittedBox(
-            fit: BoxFit.contain,
-            child: Container(
-                height: 25,
-                alignment: Alignment.center,
-                decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: Theme.of(context).bottomAppBarTheme.color),
-                child: Icon(
-                  Icons.play_arrow,
-                  color: Theme.of(context).disabledColor,
-                  size: 25,
-                ))));
+      height: 25,
+      child: FittedBox(
+        fit: BoxFit.contain,
+        child: Container(
+          height: 25,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: Theme.of(context).bottomAppBarTheme.color,
+          ),
+          child: Icon(
+            Icons.play_arrow,
+            color: Theme.of(context).disabledColor,
+            size: 25,
+          ),
+        ),
+      ),
+    );
   }
 
-  Widget _body(
-    BuildContext context,
-    List<Article> list, {
-    String? type ,
-  }) {
+  /// Page body (SliverAppBar + News List)
+  Widget _body(BuildContext context, List<Article> list, {String? type}) {
+    final safeType = (type ?? "General").toUpperCase();
+
     return CustomScrollView(
       slivers: <Widget>[
         SliverAppBar(
           centerTitle: true,
           title: Text(
-            '${type!.toUpperCase()} NEWS',
-            style: AppTheme.h2Style
-                .copyWith(color: Theme.of(context).primaryColor),
+            "$safeType NEWS",
+            style: AppTheme.h2Style.copyWith(
+              color: Theme.of(context).primaryColor,
+            ),
           ),
           backgroundColor: Theme.of(context).bottomAppBarTheme.color,
           pinned: true,
         ),
         SliverToBoxAdapter(
-            child: AspectRatio(
-                aspectRatio: 16 / 9,
-                child: PageView.builder(
-                  itemBuilder: (context, index) {
-                    return _headerNews(context, list[index]);
-                  },
-                  itemCount: 5,
-                ))),
+          child: AspectRatio(
+            aspectRatio: 16 / 9,
+            child: PageView.builder(
+              itemBuilder: (context, index) {
+                return _headerNews(context, list[index]);
+              },
+              itemCount: list.length > 5 ? 5 : list.length,
+            ),
+          ),
+        ),
         SliverList(
-            delegate: SliverChildBuilderDelegate(
-                (context, index) => NewsCard(
-                      artical: list[index],
-                      isVideoNews: true,
-                      type: type.toUpperCase(), key: null,
-                    ),
-                childCount: list.length)),
+          delegate: SliverChildBuilderDelegate(
+            (context, index) => NewsCard(
+              artical: list[index],
+              isVideoNews: true,
+              type: safeType,
+              key: null,
+            ),
+            childCount: list.length,
+          ),
+        ),
       ],
     );
   }
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final newsViewModel=  ref.watch(newsViewModelProvider(type));
+    final category = type ?? "general";
+    final newsViewModel = ref.watch(newsViewModelProvider(category));
+
     return Scaffold(
-        backgroundColor: Theme.of(context).cardTheme.color,
-        body: SafeArea(
-           child: newsViewModel.when(
+      backgroundColor: Theme.of(context).cardTheme.color,
+      body: SafeArea(
+        child: newsViewModel.when(
           data: (data) {
             if (data == null || data.isEmpty) {
-              return Text('No content avilable');
+              return const Center(child: Text('No content available'));
             } else {
-              return _body(context, data, type: type ?? 'General');
+              return _body(context, data, type: type);
             }
           },
-          error: (error, stack) => Center(child: Text('Something went wrong')),
-          loading: () => Center(child: CircularProgressIndicator()),
+          error: (error, stack) =>
+              const Center(child: Text('Something went wrong')),
+          loading: () => const Center(child: CircularProgressIndicator()),
         ),
-        ));
+      ),
+    );
   }
 }
--- lib/src/views/homePage/homePage.dart
+++ lib/src/views/homePage/homePage.dart
@@ -1,18 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 import 'package:news_app/src/commonWidget/customWidget.dart';
 import 'package:news_app/src/core/config/routes.dart';
 import 'package:news_app/src/models/article_model.dart';
 import 'package:news_app/src/theme/theme.dart';
 import 'package:news_app/src/view_models/new_view_model.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
 import 'widget/newsCard.dart';
 
 class HomePage extends ConsumerWidget {
-  String? type;
-  HomePage({super.key, this.type});
+  final String? type;
 
+  const HomePage({Key? key, this.type}) : super(key: key);
+
+  /// Header news (big featured news)
   Widget _headerNews(Article article) {
     return Builder(
       builder: (context) {
@@ -30,18 +32,13 @@ class HomePage extends ConsumerWidget {
                     : customImage(article.urlToImage ?? ''),
               ),
               Container(
-                padding: EdgeInsets.only(
-                  left: 20,
-                  right: 10,
-                  bottom: 20,
-                  top: 20,
-                ),
-                decoration: BoxDecoration(
+                padding: const EdgeInsets.only(
+                    left: 20, right: 10, bottom: 20, top: 20),
+                decoration: const BoxDecoration(
                   gradient: LinearGradient(
                     begin: Alignment.bottomCenter,
                     end: Alignment.topCenter,
                     colors: [Colors.black, Colors.transparent],
-                    // stops: [.1, .9],
                   ),
                 ),
                 child: Column(
@@ -70,13 +67,16 @@ class HomePage extends ConsumerWidget {
     );
   }
 
+  /// Body with SliverAppBar + News List
   Widget _body(BuildContext context, List<Article> list, {String? type}) {
+    final safeType = (type ?? "General").toUpperCase();
+
     return CustomScrollView(
       slivers: <Widget>[
         SliverAppBar(
           centerTitle: true,
           title: Text(
-            '${type!.toUpperCase()} NEWS',
+            "$safeType NEWS",
             style: AppTheme.h2Style.copyWith(
               color: Theme.of(context).colorScheme.surface,
             ),
@@ -87,8 +87,10 @@ class HomePage extends ConsumerWidget {
         SliverToBoxAdapter(child: _headerNews(list.first)),
         SliverList(
           delegate: SliverChildBuilderDelegate(
-            (context, index) =>
-                NewsCard(artical: list[index], type: type.toUpperCase()),
+            (context, index) => NewsCard(
+              artical: list[index],
+              type: safeType,
+            ),
             childCount: list.length,
           ),
         ),
@@ -98,6 +100,7 @@ class HomePage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    // Set system UI colors
     SystemChrome.setSystemUIOverlayStyle(
       SystemUiOverlayStyle(
         systemNavigationBarColor: Theme.of(context).colorScheme.surface,
@@ -105,7 +108,8 @@ class HomePage extends ConsumerWidget {
       ),
     );
 
-    final newsViewModel = ref.watch(newsViewModelProvider(type));
+    final category = type ?? "general"; // default if null
+    final newsViewModel = ref.watch(newsViewModelProvider(category));
 
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.surface,
@@ -113,13 +117,14 @@ class HomePage extends ConsumerWidget {
         child: newsViewModel.when(
           data: (data) {
             if (data == null || data.isEmpty) {
-              return Text('No content avilable');
+              return const Center(child: Text('No content available'));
             } else {
-              return _body(context, data, type: type ?? 'General');
+              return _body(context, data, type: type);
             }
           },
-          error: (error, stack) => Center(child: Text('Something went wrong')),
-          loading: () => Center(child: CircularProgressIndicator()),
+          error: (error, stack) =>
+              const Center(child: Text('Something went wrong')),
+          loading: () => const Center(child: CircularProgressIndicator()),
         ),
       ),
     );
